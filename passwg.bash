6e8d91352e8101706fe73c6b6ab7168bb0640962
Ciprian Dorin Craciun <ciprian@volution.ro>
2009-12-16 18:11:22 +0200

    --

--- passwg.bash
+++ passwg.bash
@@ -4,9 +4,9 @@ set -e -u -o pipefail || exit 1
 
 if test "${#}" -eq 0
 then
-	exec python2.5 ./passwg.py
+	exec python ./passwg.py
 else
-	exec python2.5 ./passwg.py "${@}"
+	exec python ./passwg.py "${@}"
 fi
 
 exit 1
