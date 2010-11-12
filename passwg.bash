27ceb212590c0c973abc7327acd4c8d247d9cde0
Ciprian Dorin Craciun <ciprian@volution.ro>
2010-11-12 12:22:40 +0200

    Updated launch script. Removed old versions and misc files.

--- passwg.bash
+++ passwg.bash
@@ -2,11 +2,15 @@
 
 set -e -u -o pipefail || exit 1
 
+sources="$( dirname "$( readlink -e "${0}" )" )"
+
+test -f "${sources}/passwg.py"
+
 if test "${#}" -eq 0
 then
-	exec python2 ./passwg.py
+	exec "${sources}/python" -E -O -O -u "${sources}/passwg.py"
 else
-	exec python2 ./passwg.py "${@}"
+	exec "${sources}/python" -E -O -O -u "${sources}/passwg.py" "${@}"
 fi
 
 exit 1
