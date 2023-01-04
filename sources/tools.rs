2304b1eff0c1d9171a69fa2ff5897c75e6464b70
Ciprian Dorin Craciun <ciprian@volution.ro>
2023-01-04 16:14:07 +0200

    [tools/main]  Add various main help related flags.

--- sources/tools.rs
+++ sources/tools.rs
@@ -52,26 +52,6 @@ pub fn main () -> MainResult<ExitCode> {
 	
 	match (_commands_refs, _arguments_refs) {
 		
-		(&["help"], _) | (&["h"], _) |
-		(&[], &["--help"]) | (&[], &["-h"]) => {
-			let mut _stdout = stdout_locked ();
-			_stdout.write (MAIN_DOC.as_bytes ()) .else_wrap (0x1741e3c0) ?;
-			Ok (ExitCode::SUCCESS)
-		}
-		
-		(&["version"], _) | (&["v"], _) |
-		(&[], &["--version"]) | (&[], &["-v"]) => {
-			let mut _stdout = stdout_locked ();
-			_stdout.write (MAIN_VERSION.as_bytes ()) .else_wrap (0x51f29851) ?;
-			Ok (ExitCode::SUCCESS)
-		}
-		
-		(&[], _) => {
-			let mut _stderr = stderr_locked ();
-			writeln! (_stderr, "[ee] [427cd93b]  expected command and arguments;  see `z-tokens help`;  aborting!") .else_wrap (0xf3c17b50) ?;
-			Ok (ExitCode::SUCCESS)
-		}
-		
 		(&["patterns"], _) | (&["p"], _) => {
 			_arguments.insert (0, String::from ("z-tokens patterns"));
 			main_patterns (_arguments)
@@ -82,18 +62,79 @@ pub fn main () -> MainResult<ExitCode> {
 			main_generate (_arguments)
 		}
 		
-		_ => {
-			let mut _stderr = stderr_locked ();
-			writeln! (_stderr, "[ee] [37d61e27]  invalid command;  see `z-tokens help`;  aborting!") .else_wrap (0x03f719cd) ?;
-			Ok (ExitCode::FAILURE)
-		}
+		(&["help"], _) | (&["h"], _) |
+		(&[], &["--help"]) | (&[], &["-h"]) |
+		(&[], &["--help-text"]) =>
+			print_and_exit (&[
+						HELP_HEADER_TEXT,
+						HELP_MAIN_TEXT.trim_matches ('\n'),
+						"\n",
+						HELP_FOOTER_TEXT,
+					], true),
+		
+		(&["version"], _) | (&["v"], _) |
+		(&[], &["--version"]) | (&[], &["-v"]) =>
+			print_and_exit (&[BUILD_VERSION], true),
+		
+		(&[], &["--readme"]) |
+		(&[], &["--readme-text"]) =>
+			print_and_exit (&[README_TEXT], true),
+		(&[], &["--readme-html"]) =>
+			print_and_exit (&[README_HTML], true),
+		
+		(&[], &["--sbom"]) |
+		(&[], &["--sbom-text"]) =>
+			print_and_exit (&[SBOM_TEXT], true),
+		(&[], &["--sbom-html"]) =>
+			print_and_exit (&[SBOM_HTML], true),
+		(&[], &["--sbom-json"]) =>
+			print_and_exit (&[SBOM_JSON], true),
+		
+		(&[], _) =>
+			print_and_exit (&["[ee] [427cd93b]  expected command and arguments;  see `z-tokens help`;  aborting!"], false),
+		
+		_ =>
+			print_and_exit (&["[ee] [37d61e27]  invalid command;  see `z-tokens help`;  aborting!"], false),
 	}
 }
 
 
 
 
-static MAIN_DOC : &'static str = include_str! ("./tools_manuals/main--help.txt");
+fn print_and_exit (_chunks : &[&str], _success : bool) -> MainResult<ExitCode> {
+	
+	let mut _stream = BufWriter::with_capacity (1024 * 1024, stdout_locked ());
+	
+	for _chunk in _chunks {
+		_stream.write (_chunk.as_bytes ()) .else_wrap (0x4c5c446d) ?;
+	}
+	
+	_stream.into_inner () .else_replace (0xbbc5724c) ?;
+	
+	if _success {
+		Ok (ExitCode::SUCCESS)
+	} else {
+		Ok (ExitCode::FAILURE)
+	}
+}
 
-static MAIN_VERSION : &'static str = include_str! ("./tools_build/version.txt");
+
+
+
+static HELP_MAIN_TEXT : &'static str = include_str! ("./.embedded/manuals/main--help.txt");
+
+static HELP_HEADER_TEXT : &'static str = include_str! ("./.embedded/manuals/help--header.txt");
+static HELP_FOOTER_TEXT : &'static str = include_str! ("./.embedded/manuals/help--footer.txt");
+
+
+static README_TEXT : &'static str = include_str! ("./.embedded/readme/readme.txt");
+static README_HTML : &'static str = include_str! ("./.embedded/readme/readme.html");
+
+
+static SBOM_TEXT : &'static str = include_str! ("./.embedded/sbom/sbom.txt");
+static SBOM_HTML : &'static str = include_str! ("./.embedded/sbom/sbom.html");
+static SBOM_JSON : &'static str = include_str! ("./.embedded/sbom/sbom.json");
+
+
+static BUILD_VERSION : &'static str = include_str! ("./.embedded/build/version.txt");
 
