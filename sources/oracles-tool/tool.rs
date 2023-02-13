20642a4634d22ba132e6c7861635364931282f64
Ciprian Dorin Craciun <ciprian@volution.ro>
2023-02-13 12:53:07 +0200

    [oracles]  Add initial proof-of-concept token usage code.

--- sources/oracles-tool/tool.rs
+++ sources/oracles-tool/tool.rs
@@ -12,6 +12,7 @@ use ::std::eprintln;
 
 mod crtk {
 	
+	#[ allow (unused_imports) ]
 	pub(super) use ::cryptoki::{
 			context::Pkcs11 as Provider,
 			context::CInitializeArgs as ProviderArguments,
@@ -22,6 +23,7 @@ mod crtk {
 			object::KeyType,
 			object::ObjectClass,
 			object::ObjectHandle,
+			mechanism::Mechanism,
 		};
 	
 	pub(super) use ::cryptoki_sys::{
@@ -45,18 +47,21 @@ define_error! (pub MainError, result : MainResult);
 #[ derive (Debug) ]
 struct RsaKey {
 	slot_id : u64,
-	id : Option<String>,
-	label : Option<String>,
-	modulus_bits : usize,
+	id : String,
+	id_data : Option<Vec<u8>>,
+	label : String,
+	label_data : Option<Vec<u8>>,
+	modulus_bits : u64,
+	modulus_data : Vec<u8>,
+	exponent_data : Vec<u8>,
 	has_private : bool,
 	has_public : bool,
 	supports_decrypt : bool,
 	supports_encrypt : bool,
 	supports_sign : bool,
 	supports_verify : bool,
-	handle_slot : Option<crtk::Slot>,
-	handle_private : Option<crtk::ObjectHandle>,
-	handle_public : Option<crtk::ObjectHandle>,
+	public_key : Option<::rsa::RsaPublicKey>,
+	public_key_pem : Option<String>,
 }
 
 
@@ -68,9 +73,11 @@ struct RsaKey {
 
 pub fn main (_arguments : Vec<String>) -> MainResult<ExitCode> {
 	
+	
 	if _arguments.len () != 1 {
 		fail! (0xb1bda4b1);
 	}
+	
 	
 	let _print_all = false;
 	let _print_found = false || _print_all;
@@ -79,10 +86,11 @@ pub fn main (_arguments : Vec<String>) -> MainResult<ExitCode> {
 	let _print_token_details = false || _print_all;
 	let _print_object_details = false || _print_all;
 	let _print_mechanism_details = false || _print_all;
-	let _print_keys = true || _print_all;
+	let _print_keys = false || _print_all;
+	let _print_keys_pem = false || _print_all;
 	
 	
-	let mut _rsa_keys : HashMap<(u64, Option<String>, Option<String>), RsaKey> = HashMap::new ();
+	let mut _rsa_keys : HashMap<(u64, Option<Vec<u8>>, Option<Vec<u8>>), RsaKey> = HashMap::new ();
 	
 	
 	let _provider_library = Path::new ("/usr/lib64/pkcs11/libsofthsm2.so");
@@ -174,13 +182,13 @@ pub fn main (_arguments : Vec<String>) -> MainResult<ExitCode> {
 		}
 		
 		
-		let mut _session = _provider.open_rw_session (_slot) .else_wrap (0x61eeccf1) ?;
+		let mut _session = _provider.open_ro_session (_slot) .else_wrap (0x61eeccf1) ?;
 		
 		_session.login (crtk::UserType::User, Some (_slot_pin)) .else_wrap (0xe928680a) ?;
 		
 		
 		let _objects = _session.find_objects (&[]) .else_wrap (0x9e90942c) ?;
-		for (_object_index, _object) in _objects.into_iter () .enumerate () {
+		for (_object_index, _object_handle) in _objects.into_iter () .enumerate () {
 			
 			if _print_object_details {
 				eprintln! ("[--]");
@@ -191,9 +199,11 @@ pub fn main (_arguments : Vec<String>) -> MainResult<ExitCode> {
 			_attribute_types.extend_from_slice (&[
 					crtk::AttributeType::Class,
 					crtk::AttributeType::KeyType,
-					crtk::AttributeType::ModulusBits,
 					crtk::AttributeType::Id,
 					crtk::AttributeType::Label,
+					crtk::AttributeType::ModulusBits,
+					crtk::AttributeType::Modulus,
+					crtk::AttributeType::PublicExponent,
 					crtk::AttributeType::Encrypt,
 					crtk::AttributeType::Decrypt,
 					crtk::AttributeType::Sign,
@@ -217,15 +227,19 @@ pub fn main (_arguments : Vec<String>) -> MainResult<ExitCode> {
 			let mut _is_public = false;
 			let mut _is_private = false;
 			let mut _is_rsa = false;
-			let mut _id = None;
-			let mut _label = None;
+			let mut _id_string = None;
+			let mut _id_data = None;
+			let mut _label_string = None;
+			let mut _label_data = None;
 			let mut _rsa_modulus_bits = None;
+			let mut _rsa_modulus_data = None;
+			let mut _rsa_exponent_data = None;
 			let mut _supports_decrypt = None;
 			let mut _supports_encrypt = None;
 			let mut _supports_sign = None;
 			let mut _supports_verify = None;
 			
-			let _attribute_details = if let Ok (_attribute_details) = _session.get_attributes (_object, &_attribute_types) {
+			let _attribute_details = if let Ok (_attribute_details) = _session.get_attributes (_object_handle, &_attribute_types) {
 				_attribute_details
 			} else {
 				if _print_object_details {
@@ -268,30 +282,47 @@ pub fn main (_arguments : Vec<String>) -> MainResult<ExitCode> {
 						if _print_object_details {
 							eprintln! ("[>>] [950ae1f9]  object {:08x} / {}:  modulus-bits `{}`;", _slot_id, _object_index, _modulus_bits);
 						}
-						_rsa_modulus_bits = Some (_modulus_bits);
+						if _is_rsa {
+							_rsa_modulus_bits = Some (_modulus_bits);
+						}
+					}
+					crtk::Attribute::Modulus (_modulus_data) => {
+						if _print_object_details {
+							eprintln! ("[>>] [1ac0cf41]  object {:08x} / {}:  modulus-data size {};", _slot_id, _object_index, _modulus_data.len ());
+						}
+						if _is_rsa {
+							_rsa_modulus_data = Some (_modulus_data.clone ());
+						}
+					}
+					crtk::Attribute::PublicExponent (_exponent_data) => {
+						if _print_object_details {
+							eprintln! ("[>>] [a5e4cc4e]  object {:08x} / {}:  exponent-data size {};", _slot_id, _object_index, _exponent_data.len ());
+						}
+						if _is_rsa {
+							_rsa_exponent_data = Some (_exponent_data.clone ());
+						}
 					}
 					
-					crtk::Attribute::Id (_id_data) => {
-						let _id_0 = {
-							let mut _buffer = String::with_capacity (_id_data.len () * 2);
-							for _byte in _id_data {
-								write! (_buffer, "{:02x}", _byte) .else_wrap (0x087b8b04) ?;
-							}
-							_buffer
-						};
+					crtk::Attribute::Id (_id_data_0) => {
+						let _id_string_0 = bytes_to_string (&_id_data_0);
 						if _print_object_details {
-							eprintln! ("[>>] [23d27f54]  object {:08x} / {}:  id `{}`;", _slot_id, _object_index, _id_0);
+							eprintln! ("[>>] [23d27f54]  object {:08x} / {}:  id `{}`;", _slot_id, _object_index, _id_string_0);
 						}
-						_id = if ! _id_0.is_empty () { Some (_id_0) } else { None };
+						_id_data = Some (_id_data_0.clone ());
+						_id_string = Some (_id_string_0);
 					},
 					
-					crtk::Attribute::Label (_label_data) => {
-						let _label_0 = str::from_utf8 (&_label_data) .else_wrap (0x9dbd9c6b) ?;
-						let _label_0 = String::from (_label_0);
+					crtk::Attribute::Label (_label_data_0) => {
+						let _label_string_0 = if let Ok (_label_string_0) = str::from_utf8 (&_label_data_0) {
+								String::from (_label_string_0)
+							} else {
+								bytes_to_string (&_label_data_0)
+							};
 						if _print_object_details {
-							eprintln! ("[>>] [df90fc52]  object {:08x} / {}:  label `{}`;", _slot_id, _object_index, _label_0);
+							eprintln! ("[>>] [df90fc52]  object {:08x} / {}:  label `{}`;", _slot_id, _object_index, _label_string_0);
 						}
-						_label = if ! _label_0.is_empty () { Some (_label_0) } else { None };
+						_label_data = Some (_label_data_0.clone ());
+						_label_string = Some (_label_string_0);
 					},
 					
 					crtk::Attribute::Encrypt (_supported) =>
@@ -328,25 +359,40 @@ pub fn main (_arguments : Vec<String>) -> MainResult<ExitCode> {
 			
 			if _is_rsa {
 				
-				let _key = _rsa_keys.entry ((_slot_id, _id.clone (), _label.clone ())) .or_default ();
-				
-				_key.id = _id;
-				_key.label = _label;
+				let _key = _rsa_keys.entry ((_slot_id, _id_data.clone (), _label_data.clone ())) .or_default ();
 				
 				_key.slot_id = _slot_id;
-				_key.handle_slot = Some (_slot.clone ());
+				
 				if _is_private {
 					_key.has_private = true;
-					_key.handle_private = Some (_object);
 				}
 				if _is_public {
 					_key.has_public = true;
-					_key.handle_public = Some (_object);
+				}
+				
+				if let Some (_id_data) = _id_data {
+					_key.id_data = Some (_id_data.clone ());
+				}
+				if let Some (_id_string) = _id_string {
+					_key.id = _id_string.clone ();
+				}
+				if let Some (_label_data) = _label_data {
+					_key.label_data = Some (_label_data.clone ());
+				}
+				if let Some (_label_string) = _label_string {
+					_key.label = _label_string.clone ();
 				}
 				
 				if let Some (_rsa_modulus_bits) = _rsa_modulus_bits {
 					_key.modulus_bits = _rsa_modulus_bits.try_into () .else_wrap (0x000d5627) ?;
 				}
+				if let Some (_rsa_modulus_data) = _rsa_modulus_data {
+					_key.modulus_data = _rsa_modulus_data.clone ();
+				}
+				if let Some (_rsa_exponent_data) = _rsa_exponent_data {
+					_key.exponent_data = _rsa_exponent_data.clone ();
+				}
+				
 				if let Some (_supports_decrypt) = _supports_decrypt {
 					_key.supports_decrypt = _key.supports_decrypt || _supports_decrypt;
 				}
@@ -371,26 +417,151 @@ pub fn main (_arguments : Vec<String>) -> MainResult<ExitCode> {
 	}
 	
 	
-	_provider.finalize ();
-	
-	
-	for _key in _rsa_keys.values () {
-		eprintln! ("[>>] [1f034f51]  RSA:  slot {:08x} ({}), id `{}`, label `{}`, bits {}, private {}, public {}, decrypt {}, encrypt {}, sign {}, verify {};",
-				_key.slot_id, _key.slot_id,
-				_key.id.as_ref () .map (String::as_str) .unwrap_or (""),
-				_key.label.as_ref () .map (String::as_str) .unwrap_or (""),
-				_key.modulus_bits,
-				_key.has_private,
-				_key.has_public,
-				_key.supports_decrypt,
-				_key.supports_encrypt,
-				_key.supports_sign,
-				_key.supports_verify,
-			);
+	for _key in _rsa_keys.values_mut () {
+		
+		if _print_keys {
+			eprintln! ("[>>] [1f034f51]  RSA:  slot {:08x} ({}), id `{}`, label `{}`, bits {}, private {}, public {}, decrypt {}, encrypt {}, sign {}, verify {};",
+					_key.slot_id, _key.slot_id,
+					_key.id,
+					_key.label,
+					_key.modulus_bits,
+					_key.has_private,
+					_key.has_public,
+					_key.supports_decrypt,
+					_key.supports_encrypt,
+					_key.supports_sign,
+					_key.supports_verify,
+				);
+		}
+		
+		if (_key.modulus_bits as usize) != (_key.modulus_data.len () * 8) {
+			if _print_keys {
+				eprintln! ("[ww] [2a7fd100]    -> mismatched RSA modulus bits and data size!");
+			}
+			continue;
+		}
+		if _key.modulus_data.is_empty () {
+			if _print_keys {
+				eprintln! ("[ww] [1de0fb30]    -> empty RSA modulus data!");
+			}
+			continue;
+		}
+		if _key.exponent_data.is_empty () {
+			if _print_keys {
+				eprintln! ("[ww] [fc7901df]    -> empty RSA exponent data!");
+			}
+			continue;
+		}
+		
+		let _modulus = ::rsa::BigUint::from_bytes_be (&_key.modulus_data);
+		let _exponent = ::rsa::BigUint::from_bytes_be (&_key.exponent_data);
+		let _public_key = match ::rsa::RsaPublicKey::new (_modulus, _exponent) {
+			Ok (_public_key) =>
+				_public_key,
+			Err (_error) => {
+				if _print_keys {
+					eprintln! ("[ww] [bf8432ac]    -> invalid RSA public key:  {}!", _error);
+				}
+				continue;
+			}
+		};
+		
+		let _public_key_pem = ::rsa::pkcs8::EncodePublicKey::to_public_key_pem (&_public_key, ::pkcs1::LineEnding::LF) .else_wrap (0xe18a461e) ?;
+		if _print_keys && _print_keys_pem {
+			eprintln! ("[>>] [16728ad7]    -> RSA public key PEM:\n{}[--]", _public_key_pem);
+		}
+		
+		_key.public_key = Some (_public_key);
+		_key.public_key_pem = Some (_public_key_pem);
 	}
 	
 	
+	for _key in _rsa_keys.values () {
+		
+		if ! _key.supports_decrypt {
+			continue;
+		}
+		
+		let Some (_public_key) = _key.public_key.as_ref ()
+			else {
+				continue;
+			};
+		
+		eprintln! ("[ii] [76ffc916]  encrypting with RSA:  slot {:08x}, id `{}`, label `{}`...",
+				_key.slot_id,
+				_key.id,
+				_key.label,
+			);
+		
+		
+		let _slot = crtk::Slot::try_from (_key.slot_id) .else_wrap (0x12467a0d) ?;
+		
+		let mut _session = _provider.open_ro_session (_slot) .else_wrap (0x49a812d6) ?;
+		
+		_session.login (crtk::UserType::User, Some (_slot_pin)) .else_wrap (0x12efc15f) ?;
+		
+		
+		let mut _private_attributes = vec! [
+				crtk::Attribute::Class (crtk::ObjectClass::PRIVATE_KEY),
+				crtk::Attribute::KeyType (crtk::KeyType::RSA),
+				crtk::Attribute::Sign (true),
+			];
+		
+		if let Some (ref _id_data) = _key.id_data {
+			_private_attributes.push (crtk::Attribute::Id (_id_data.clone ()));
+		}
+		if let Some (ref _label_data) = _key.label_data {
+			_private_attributes.push (crtk::Attribute::Label (_label_data.clone ()));
+		}
+		
+		let _private_handles = _session.find_objects (&_private_attributes) .else_wrap (0x75913f5f) ?;
+		let _private_handle = match _private_handles.as_slice () {
+			&[] =>
+				fail! (0x412b319d),
+			&[_private_handle] =>
+				_private_handle.clone (),
+			&[_, _, ..] =>
+				fail! (0x41b13171),
+		};
+		
+		
+		let _input_size = (_key.modulus_bits / 8) as usize;
+		let mut _input_data = vec! [0xffu8; _input_size];
+		
+		// eprintln! ("[>>] [9ad03ccd]    -> input-data ({} / {}):  {:02x?}", _input_data.len (), _input_data.len () * 8, _input_data.as_slice ());
+		
+		
+		for _round in 0 .. 16 {
+			
+			let _output_data = _session.sign (&crtk::Mechanism::Sha512RsaPkcs, _private_handle, &_input_data) .else_wrap (0x723ea89b) ?;
+			
+			eprintln! ("[>>] [9ad03ccd]    -> (round {}) output-data ({} / {}):  {:02x?}", _round + 1, _output_data.len (), _output_data.len () * 8, & _output_data.as_slice () [..16]);
+		}
+		
+		
+		_session.logout () .else_wrap (0xfa1ce8bf) ?;
+	}
+	
+	
+	_provider.finalize ();
+	
+	
 	Ok (ExitCode::SUCCESS)
+}
+
+
+
+
+
+
+
+
+fn bytes_to_string (_bytes : &[u8]) -> String {
+	let mut _buffer = String::with_capacity (_bytes.len () * 2);
+	for _byte in _bytes {
+		write! (_buffer, "{:02x}", _byte) .else_panic (0x087b8b04);
+	}
+	_buffer
 }
 
 
