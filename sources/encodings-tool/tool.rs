0823419bcccf5518e36b118c7ac4f4477a42b869
Ciprian Dorin Craciun <ciprian@volution.ro>
2023-12-30 14:04:18 +0200

    [misc]  Minor update.

--- sources/encodings-tool/tool.rs
+++ sources/encodings-tool/tool.rs
@@ -15,9 +15,9 @@ define_error! (pub MainError, result : MainResult);
 pub fn main_encode (_arguments : Vec<String>) -> MainResult<ExitCode> {
 	
 	{
-		let mut _parser = create_parser () .else_wrap (0x2e40868a) ?;
+		let mut _flags = create_flags () .else_wrap (0x2e40868a) ?;
 		
-		if execute_parser (_parser, _arguments) .else_wrap (0x75b0a124) ? {
+		if execute_flags (_flags, _arguments) .else_wrap (0x75b0a124) ? {
 			return Ok (ExitCode::SUCCESS);
 		}
 	}
@@ -31,9 +31,9 @@ pub fn main_encode (_arguments : Vec<String>) -> MainResult<ExitCode> {
 pub fn main_decode (_arguments : Vec<String>) -> MainResult<ExitCode> {
 	
 	{
-		let mut _parser = create_parser () .else_wrap (0xe6cc7904) ?;
+		let mut _flags = create_flags () .else_wrap (0xe6cc7904) ?;
 		
-		if execute_parser (_parser, _arguments) .else_wrap (0x53f611ac) ? {
+		if execute_flags (_flags, _arguments) .else_wrap (0x53f611ac) ? {
 			return Ok (ExitCode::SUCCESS);
 		}
 	}
