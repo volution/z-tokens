762a8c33cef5d5f8987a8804ad0c2914524cb66b
Ciprian Dorin Craciun <ciprian@volution.ro>
2024-01-04 18:51:57 +0200

    [cargo]  Drop `argparse` dependency.

--- sources/runtime/flags.rs
+++ sources/runtime/flags.rs
@@ -6,19 +6,6 @@ use ::vrl_errors::*;
 
 
 
-pub use ::argparse::{
-		ArgumentParser as ArgParser,
-		Store as ArgStore,
-		StoreTrue as ArgStoreTrue,
-		StoreFalse as ArgStoreFalse,
-		StoreConst as ArgStoreConst,
-		StoreOption as ArgStoreOption,
-		Collect as ArgPush,
-		PushConst as ArgPushConst,
-		self as argparse,
-	};
-
-
 pub use ::vrl_cli_arguments::*;
 
 
@@ -32,28 +19,11 @@ define_error! (pub FlagsError, result : FlagsResult);
 
 
 
-pub fn create_parser <'a> () -> FlagsResult<ArgParser<'a>> {
-	Ok (ArgParser::new ())
-}
-
 pub fn create_flags <'a> () -> FlagsResult<FlagsParserBuilder<'a>> {
 	Ok (FlagsParserBuilder::new ())
 }
 
 
-
-
-pub fn execute_parser (_parser : ArgParser, _arguments : Vec<String>) -> FlagsResult<bool> {
-	
-	match _parser.parse (_arguments, &mut stdout_locked (), &mut stderr_locked ()) {
-		Ok (()) =>
-			Ok (false),
-		Err (0) =>
-			Ok (true),
-		Err (_error) =>
-			fail! (0x0f71ad86),
-	}
-}
 
 
 pub fn execute_flags (mut _parser : FlagsParserBuilder, mut _arguments : Vec<String>) -> FlagsResult<bool> {
