fc881cd8c9cc3ab4369daa961f3d6338ef08aab3
Ciprian Dorin Craciun <ciprian@volution.ro>
2024-10-12 16:27:08 +0300

    [exchange]  Add feature gate for SSH wrapper.

--- sources/exchange/lib.rs
+++ sources/exchange/lib.rs
@@ -10,11 +10,11 @@ pub mod coding;
 pub mod oracles;
 
 
-#[ cfg (not (any (target_os = "wasi", target_os = "windows"))) ]
+#[ cfg (all (feature = "zt-exchange-ssh", not (any (target_os = "wasi", target_os = "windows")))) ]
 #[ path = "./ssh_enabled.rs" ]
 pub mod ssh;
 
-#[ cfg (any (target_os = "wasi", target_os = "windows")) ]
+#[ cfg (any (not (feature = "zt-exchange-ssh"), target_os = "wasi", target_os = "windows")) ]
 #[ path = "./ssh_disabled.rs" ]
 pub mod ssh;
 
